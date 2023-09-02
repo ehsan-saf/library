9aaec7ef8fdc03441bee13123dc6aee1bd65313b
M.Ehsan <124778596+ehsan-saf@users.noreply.github.com>
2023-09-02 16:34:38 +0330

    Code to show a book in grid

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const booksGrid = document.querySelector(".books-grid");
+const book = document.querySelector(".blueprint").cloneNode(true);
+book.classList.remove("blueprint");
+book.classList.add("book");
+
 const myLibrary = [];
 
 function Book(title, author, pages, hasRead) {
@@ -8,5 +13,25 @@ function Book(title, author, pages, hasRead) {
 }
 
 function addBookToLibrary(newBook) {
-    
+    myLibrary.push(newBook);
 }
+
+const book1 = new Book("book1", "author1", 25, true);
+addBookToLibrary(book1);
+displayBooks();
+
+function displayBooks() {
+    myLibrary.forEach(b => {
+        book.querySelector(".title").textContent = b.title;
+        book.querySelector(".author").textContent = b.author;
+        book.querySelector(".page").textContent = b.pages;
+        // book.querySelector(".status").src = "read.png";
+        booksGrid.appendChild(book);
+    });
+}
+
+
+
+
+
+
